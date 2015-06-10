5171de8c38e30e92a9263ebb82bee1d686453fe1
pangjianhai <821761509@qq.com>
2015-06-10 15:47:01 +0800

    sub

--- map_location/src/cn/com/maplocation/MainActivity.java
+++ map_location/src/cn/com/maplocation/MainActivity.java
@@ -1,34 +1,79 @@
 package cn.com.maplocation;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private TextView position_text_view;
+
+	private LocationManager lm;
+
+	private String provider;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		position_text_view = (TextView) findViewById(R.id.position_text_view);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		List<String> providerList = lm.getProviders(true);
+		if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+			provider = LocationManager.GPS_PROVIDER;
+		} else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+			provider = LocationManager.NETWORK_PROVIDER;
+		} else {
+			Toast.makeText(getApplicationContext(), "没有地理位置提供器",
+					Toast.LENGTH_SHORT).show();
+			return;
 		}
-		return super.onOptionsItemSelected(item);
+		System.out.println("--------------------------->>>>>>>>>>>"+provider);
+		Location location = lm.getLastKnownLocation(provider);
+		if (location != null) {
+			showLocation(location);
+		}
+		System.out.println("--------------------------->>>>>>>>>>>"+location);
+		lm.requestLocationUpdates(provider, 1000, 1, ll);
 	}
+
+	LocationListener ll = new LocationListener() {
+
+		@Override
+		public void onLocationChanged(Location location) {
+			System.out.println("onLocationChanged--------------------------->>>>>>>>>>>"+location);
+			showLocation(location);
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+
+		}
+	};
+
+	private void showLocation(Location l) {
+		String lat = l.getLatitude() + "";
+		String lon = l.getLongitude() + "";
+		String str = lat + "\r\n" + lon;
+		position_text_view.setText(str);
+	}
+
 }
